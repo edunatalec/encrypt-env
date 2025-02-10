fce28134554797bcf16d94b1737a86b4f087a724
Eduardo Natale Costa <edunatalec@gmail.com>
2025-02-10 14:57:03 -0300

    test: increase coverage

--- test/src/utils/map.utils_test.dart
+++ test/src/utils/map.utils_test.dart
@@ -26,4 +26,57 @@ void main() {
 
     expect(a.merge(b), matcher);
   });
+
+  test('prettify function should format a Map as a JSON string', () {
+    final Map data = {
+      'name': 'Alice',
+      'age': 25,
+      'address': {'city': 'New York', 'zip': '10001'},
+    };
+
+    final String expectedJson = '''{
+  "name": "Alice",
+  "age": 25,
+  "address": {
+    "city": "New York",
+    "zip": "10001"
+  }
+}''';
+
+    expect(data.prettify(), expectedJson);
+  });
+
+  test('prettify function should return an empty JSON object for an empty map',
+      () {
+    final Map emptyMap = {};
+
+    expect(emptyMap.prettify(), '{}');
+  });
+
+  test('prettify function should format nested maps correctly', () {
+    final Map nestedMap = {
+      'person': {
+        'name': 'John',
+        'details': {
+          'age': 30,
+          'languages': ['Dart', 'Python']
+        }
+      }
+    };
+
+    final String expectedJson = '''{
+  "person": {
+    "name": "John",
+    "details": {
+      "age": 30,
+      "languages": [
+        "Dart",
+        "Python"
+      ]
+    }
+  }
+}''';
+
+    expect(nestedMap.prettify(), expectedJson);
+  });
 }
