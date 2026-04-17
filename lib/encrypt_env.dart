1e24c49cfa5802a5b8f9a5b943b3c43513b4ee3b
Eduardo Natale Costa <edunatalec@gmail.com>
2026-04-16 22:16:29 -0300

    docs: add api docs

--- lib/encrypt_env.dart
+++ lib/encrypt_env.dart
@@ -1,1 +1,7 @@
+/// Generate obfuscated or encrypted Dart files from YAML/JSON configuration.
+///
+/// `encrypt_env` is a command-line tool. Install it globally with
+/// `dart pub global activate encrypt_env` and run `encrypt_env --help` to get
+/// started. See the package README for full usage:
+/// <https://github.com/edunatalec/encrypt-env>.
 library;
